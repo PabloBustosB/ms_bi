f68dc07e7ac9a9f6573cacc9db94fb498f9d3167
Pablo Ramiro Bustos Belzu <pablobb.ram@gmail.com>
2023-06-14 12:12:01 -0400

    Implementado receta y medicamento

--- index.php
+++ index.php
@@ -68,6 +68,41 @@
     Flight::json($datos);
   });
 
+  Flight::route('POST /recetas', function () {
+    $recomendacion = Flight::request()->data->recomendacion;
+    $idconsulta = Flight::request()->data->idconsulta;
+    $sql = "INSERT INTO receta(recomendacion, idconsulta) VALUES (?,?);";
+    $query = Flight::db()->prepare($sql);
+    $query->bindparam(1, $recomendacion);
+    $query->bindparam(2, $idconsulta);
+    $query->execute();
+    Flight::json(["Receta Registrada"]);
+  });
+
+  Flight::route('GET /medicamento', function () {
+    $query = Flight::db()->prepare("SELECT * FROM medicamento");
+    $query->execute();
+    $datos = $query->fetchAll(PDO::FETCH_ASSOC);
+    Flight::json($datos);
+  });
+
+  Flight::route('POST /medicamento', function () {
+    $nombre = Flight::request()->data->nombre;
+    $cantidad = Flight::request()->data->cantidad;
+    $indicacion = Flight::request()->data->indicacion;
+    $idreceta= Flight::request()->data->idreceta;
+    $sql = "INSERT INTO medicamento(nombre, cantidad, indicacion, idreceta) VALUES (?,?,?,?);";
+    $query = Flight::db()->prepare($sql);
+    $query->bindparam(1, $nombre);
+    $query->bindparam(2, $cantidad);
+    $query->bindparam(3, $indicacion);
+    $query->bindparam(4, $idreceta);
+    $query->execute();
+    Flight::json(["Medicamento Registrada"]);
+  });
+
+  
+
 Flight::before('json', function () {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET,PUT,POST,DELETE');
